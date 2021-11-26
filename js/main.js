4c48589a14e0dffefb10a5fe36bef120ae6ae206
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2021-11-26 11:16:31 -0300

    primera integradora del curso

---
+++ js/main.js
@@ -0,0 +1,3 @@
+var saludo = prompt("Ingrese su nombre: ");
+
+alert(`${saludo} Init te da la bienvenida!!`)
---
+++ js/suscripcion.js
@@ -0,0 +1,9 @@
+var suscripcion = prompt("¿Desea susucribirse a nuestra newsletter para mantenerse informado de nuestros cursos? (si/no)");
+
+if(suscripcion == "si"){
+    prompt("Digite su mail: ");
+    alert("Gracias por suscribite a nuestra newsletter:).")
+}
+else{
+    alert("En otra ocacion sera:')");
+}
